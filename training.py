e15bf1baabdb07e6fc3beb47afe1b10032a20bca
ozgur <ozgurrkara99@gmail.com>
2023-11-20 20:18:51 +0000

    training implemented

--- training.py
+++ training.py
@@ -1,10 +1,10 @@
+import torch
 from stable_diffusion_hspace import StableDiffusionPipelineHspace
 from unet_hspace import UNet2DConditionModelHSpace
-import torch
 from pathlib import Path
-from utils import save_images_and_hspace
-import time
-PATH = "stable-diffusion-v1-4"
+import torch.nn.functional as F
+
+PATH = "/home/hice1/mnigam9/scratch/cache/stable-diffusion-v1-4"
 PATH = Path(PATH).expanduser()
 
 def setup_hspace_stable_diffusion(PATH):
@@ -23,6 +23,7 @@ def setup_hspace_stable_diffusion(PATH):
         subfolder = "unet",
         torch_dtype=torch.float16 
     )
+    freeze_params(hspace_unet)
     hspace_pipe = StableDiffusionPipelineHspace.from_pretrained(
         PATH, 
         torch_dtype = torch.float16, 
@@ -54,31 +55,37 @@ def get_config():
         CONFIG["latent_dims"] = 2    
     return CONFIG
 
-if __name__=="__main__":
+
+def freeze_params(unet):
+    for param in unet.parameters():
+        param.requires_grad = False
+    for param in unet.deltablock.parameters():
+        param.requires_grad = True
+
+def compute_kl_divergence(generated_distribution, target_distribution):
+    target_distribution = target_distribution / target_distribution.sum()
+    target_log_prob = torch.log(target_distribution)
+    kl_div = F.kl_div(generated_distribution, target_log_prob, reduction='batchmean')
+    return kl_div
+
+if __name__=='__main__':
+    
     hspace_pipe = setup_hspace_stable_diffusion(PATH)
     config = get_config()
-    # images here is of type StableDiffusionPipelineOutput. To access images, use images.images
-    start = time.time()
-    images, hspace = hspace_pipe(
-        prompt = config["prompts"],
-        num_inference_steps = config["num_inference_steps"],
-        num_images_per_prompt = config["num_images_per_prompt"]    
-    )
-    end = time.time()
-    print("Time taken: ", end-start)
+
+    optimizer = torch.optim.SGD(hspace_pipe.unet.parameters(), lr=0.001, momentum=0.9)
     
-    # breakpoint()
-    print("CONFIG: ", config)
-    print("Number of images: ", len(images.images))
-    print("Hspace dimension: ", hspace.shape)
+    for i in range(100):
+        optimizer.zero_grad()
+        images, hspace = hspace_pipe(
+            prompt = config["prompts"],
+            num_inference_steps = config["num_inference_steps"],
+            num_images_per_prompt = config["num_images_per_prompt"]    
+        )
     
-    save_images_and_hspace(
-        images.images, 
-        hspace, 
-        config["latent_dims"],
-        save_path="./outputs/",
-        print_path=False
-    )
-    
-    
-    
+        prob_dist = classify(images)
+        uniform_dist = torch.ones(images.shape[0], 2) * 0.5
+        kl_divergence = compute_kl_divergence(prob_dist, uniform_dist)
+        kl_divergence.backward()
+        optimizer.step()
+        print("KL divergence: ", kl_divergence)
