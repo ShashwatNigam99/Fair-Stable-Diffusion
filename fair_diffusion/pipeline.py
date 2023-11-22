287b747d53df939c9927c2b3710ff69f829dc3c0
xavihart <xavihart@sjtu.edu.cn>
2023-11-21 23:35:51 -0500

    ..

--- fair_diffusion/pipeline.py
+++ fair_diffusion/pipeline.py
@@ -19,7 +19,8 @@ def mp(p):
         os.makedirs(p_new)
 
 
-def gen_occupation_face(occupation='firefighter', bias=['male person', 'female person'], seed=0, num_images_per_prompt=1, output_name=None):
+
+def gen_occupation_face(occupation='firefighter', bias='gender', seed=0, num_images_per_prompt=1, output_name=None, raw=False):
 
     device='cuda'
     pipe = SemanticEditPipeline.from_pretrained(
@@ -32,10 +33,17 @@ def gen_occupation_face(occupation='firefighter', bias=['male person', 'female p
     # input image
     gen.manual_seed(seed)
     
-    edit = ['male person', 'female person']
+    
+    if bias == 'gender':
+        edit = ['male person', 'female person']
+    elif bias == 'color':
+        edit = ['white person', 'black person']
+    else:
+        raise KeyboardInterrupt('unknow bias')
+    
     
     # enhance one concept and weakness the others
-    n_properties = len(bias)
+    n_properties = len(edit)
     reverse_editing_direction = [False] * n_properties
     enhance_idx = random.randint(0, n_properties-1)
     print('ENHANCE', enhance_idx)
@@ -49,31 +57,55 @@ def gen_occupation_face(occupation='firefighter', bias=['male person', 'female p
     
 
     gen.manual_seed(seed)
-    out = pipe(prompt=prompt, generator=gen, num_images_per_prompt=num_images_per_prompt, guidance_scale=7,
-            editing_prompt=edit,       # Concepts to apply 
-            reverse_editing_direction=reverse_editing_direction, # Direction of guidance i.e. increase cars, decrease crowd, and add sunset
-            edit_warmup_steps=edit_warmup_steps, # Warmup period for each concept
-            edit_guidance_scale=edit_guidance_scale, # Guidance scale for each concept
-            edit_threshold=edit_threshold, # Threshold for each concept. Note that positive guidance needs negative thresholds and vice versa
-            edit_momentum_scale=0.5, # Momentum scale that will be added to the latent guidance
-            edit_beta1=0.6, # Momentum beta
-            edit_weights=edit_weights # Weights of the individual concepts against each other
-            )
+    
+    
+    if not raw:
+        out = pipe(prompt=prompt, generator=gen, num_images_per_prompt=num_images_per_prompt, guidance_scale=7,
+                editing_prompt=edit,       # Concepts to apply 
+                reverse_editing_direction=reverse_editing_direction, # Direction of guidance i.e. increase cars, decrease crowd, and add sunset
+                edit_warmup_steps=edit_warmup_steps, # Warmup period for each concept
+                edit_guidance_scale=edit_guidance_scale, # Guidance scale for each concept
+                edit_threshold=edit_threshold, # Threshold for each concept. Note that positive guidance needs negative thresholds and vice versa
+                edit_momentum_scale=0.5, # Momentum scale that will be added to the latent guidance
+                edit_beta1=0.6, # Momentum beta
+                edit_weights=edit_weights # Weights of the individual concepts against each other
+                )
+    else: # sd-v1.5 baseline, has bias
+        out = pipe(prompt=prompt, generator=gen, num_images_per_prompt=num_images_per_prompt, guidance_scale=7)
+
     
     images = out.images
     
     # save output
-    mp(f'out/{occupation}/')
+    method = 'fairdiff' if not raw else 'stablediff'
+    mp(f'out/{occupation}/{method}/')
     for i, img in enumerate(images):
-        img.save(f'out/{occupation}/{output_name}.png')
-        print('IMAGE OUTPUT TO:', f'out/{occupation}/{output_name}.png')
+        img.save(f'out/{occupation}/{method}/{output_name}.png')
+        print('IMAGE OUTPUT TO:', f'out/{occupation}/{method}/{output_name}.png')
 
 # test
-
 if __name__ == '__main__':
     
-    for i in range(10):
-        gen_occupation_face('cook', seed=i, output_name=f'{i}')
+    
+    occupation_list = [
+        ('firefighter', 'gender'),
+        ('accountant', 'color'),
+        ('CEO', 'gender'),
+        ('cleaner', 'gender'),
+        ('dentist', 'color'),
+        ('manager', 'gender'),
+        ('professor', 'gender'),
+        ('writer', 'gender'),
+        ('mover', 'color'),
+        ('manicurist', 'color')
+    ]
+    
+    num_for_each_prompt = 100
+    
+    for occupation, bias in occupation_list:
+        for i in range(num_for_each_prompt):
+            gen_occupation_face('occupation', bias=bias, seed=i, output_name=f'{i}', raw=False)
+            gen_occupation_face('occupation', bias=bias, seed=i, output_name=f'{i}', raw=True)
         
         
     
