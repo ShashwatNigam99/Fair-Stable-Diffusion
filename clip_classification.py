09773a05311c3cd253a20bc20a0277e88715532d
ozgur <ozgurrkara99@gmail.com>
2023-11-23 19:30:03 +0000

    working code

--- clip_classification.py
+++ clip_classification.py
@@ -9,7 +9,9 @@ class Classifier(torch.nn.Module):
         super().__init__()
         self.model = CLIPModel.from_pretrained(model_name_or_path).to(device)
         self.processor = CLIPProcessor.from_pretrained(model_name_or_path)
-
+        for param in self.model.parameters():
+            param.requires_grad = True
+        
     def forward(self, text, images):
         inputs = self.processor(text=text, images=images, return_tensors="pt", padding=True)
         inputs.to(self.model.device)
@@ -18,7 +20,7 @@ class Classifier(torch.nn.Module):
     
     def classify(self, outputs):
         probs = outputs.logits_per_image.softmax(dim=1)
-        return probs.cpu().detach().numpy()
+        return probs
     
 OUTPUTS = "outputs2/images"
 OUTPUTS = Path(OUTPUTS).expanduser()
@@ -26,14 +28,15 @@ OUTPUTS = Path(OUTPUTS).expanduser()
 def load_image(image_path):
     return Image.open(str(image_path))
 
-def classify(image_path): 
+def classify(images): 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Classifier("clip-vit-base-patch32", device)
-    path = Path(image_path)
-    image_names, images = [], []
-    for img in path.rglob('*'):
-        image_names.append(str(img))
-        images.append(load_image(img))
+    # path = Path(image_path)
+
+    # image_names, images = [], []
+    # for img in path.rglob('*'):
+    #     image_names.append(img)
+    #     images.append(load_image(img))
 
     text_classes = ["photo of a man", "photo of a woman" ]
     prompts = ["a photo of an Asian","a photo of a caucasian", "a photo of a black person", "a photo of a latin American"]
@@ -45,28 +48,57 @@ def classify(image_path):
     # for i, img in enumerate(image_names):
     #     print(img.split('/')[-1], probs[i])
     #     print(img.split('/')[-1], probs_race[i])
+    # gender_labels = ['Man', 'Woman']
+    # race_labels = ['asian', 'white', 'black', 'latino']
+
+    # # Initialize counters for each attribute
+    # gender_counts = {label: 0 for label in gender_labels}
+    # race_counts = {label: 0 for label in race_labels}
+
+    # # Iterate over each image's probabilities and count the occurrences of each attribute
+    # for g_prob, r_prob in zip(probs_gender, probs_race):
+    #     # Get the index of the highest probability for gender and race
+    #     max_g_index = np.argmax(g_prob)
+    #     max_r_index = np.argmax(r_prob)
+
+    #     # Increment the corresponding attribute count
+    #     gender_counts[gender_labels[max_g_index]] += 1
+    #     race_counts[race_labels[max_r_index]] += 1
+
+    # # Calculate the distribution percentages
+    # total_images = len(probs_gender)
+    # gender_distribution = {k: v / total_images for k, v in gender_counts.items()}
+    # race_distribution = {k: v / total_images for k, v in race_counts.items()}
+    # return gender_distribution, race_distribution
+
     gender_labels = ['Man', 'Woman']
     race_labels = ['asian', 'white', 'black', 'latino']
+    # print(probs_gender)
+    probs_gender = torch.sum(probs_gender, dim=0)
+    # print(probs_gender)
+    probs_gender /= torch.sum(probs_gender) 
+    # input(probs_gender)
 
-    # Initialize counters for each attribute
-    gender_counts = {label: 0 for label in gender_labels}
-    race_counts = {label: 0 for label in race_labels}
+    # Use PyTorch operations to find the max indices
+    # _, max_g_indices = torch.max(probs_gender, 1)
+    # _, max_r_indices = torch.max(probs_race, 1)
 
-    # Iterate over each image's probabilities and count the occurrences of each attribute
-    for g_prob, r_prob in zip(probs_gender, probs_race):
-        # Get the index of the highest probability for gender and race
-        max_g_index = np.argmax(g_prob)
-        max_r_index = np.argmax(r_prob)
+    # Initialize tensors to store counts
+    # gender_counts = torch.zeros(len(gender_labels)).to(device)
+    # race_counts = torch.zeros(len(race_labels)).to(device)
 
-        # Increment the corresponding attribute count
-        gender_counts[gender_labels[max_g_index]] += 1
-        race_counts[race_labels[max_r_index]] += 1
+    # # Count occurrences of each attribute
+    # for i in range(len(gender_labels)):
+    #     gender_counts[i] = (max_g_indices == i).sum().float()
 
-    # Calculate the distribution percentages
-    total_images = len(probs_gender)
-    gender_distribution = {k: v / total_images for k, v in gender_counts.items()}
-    race_distribution = {k: v / total_images for k, v in race_counts.items()}
-    return gender_distribution, race_distribution
+    # for i in range(len(race_labels)):
+    #     race_counts[i] = (max_r_indices == i).sum().float()
+
+    # # Calculate the distribution percentages
+    # gender_distribution = gender_counts / probs_gender.shape[0]
+    # race_distribution = race_counts / probs_race.shape[0]
+    # return  gender_distribution, race_distribution
+    return probs_gender
 
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
--- training.py
+++ training.py
@@ -51,9 +51,9 @@ def setup_hspace_stable_diffusion(PATH):
     return hspace_pipe
 
 CONFIG = {
-    "prompts": ["Photo portrait of a doctor", "Photo portrait of a teacher", "Photo portrait of a lawyer"],    
+    "prompts": ["Photo portrait of a doctor"],    
     "num_inference_steps": 50,
-    "num_images_per_prompt": 2,
+    "num_images_per_prompt": 18,
     "classifier_free_guidance": True
 }
 
@@ -66,7 +66,7 @@ def get_config():
 
 def freeze_params(unet):
     for param in unet.parameters():
-        param.requires_grad = False
+        param.requires_grad = True
     for param in unet.deltablock.parameters():
         param.requires_grad = True
 
@@ -81,8 +81,8 @@ if __name__=='__main__':
     hspace_pipe = setup_hspace_stable_diffusion(PATH)
     config = get_config()
 
-    optimizer = torch.optim.SGD(hspace_pipe.unet.parameters(), lr=0.001, momentum=0.9)
-    
+    optimizer = torch.optim.SGD(hspace_pipe.unet.deltablock.parameters(), lr=10000, momentum=0.9)
+    hspace_pipe.unet.deltablock.train()
     for i in range(100):
         optimizer.zero_grad()
         images, hspace = hspace_pipe(
@@ -90,24 +90,31 @@ if __name__=='__main__':
             num_inference_steps = config["num_inference_steps"],
             num_images_per_prompt = config["num_images_per_prompt"]    
         )
-        images_path = "outputs2/images/"
-        os.makedirs(images_path, exist_ok=True)
+        # images_path = "outputs2/images/"
+        # os.makedirs(images_path, exist_ok=True)
 
-        hh = 0
-        for h in images.images:
-            h.save(f'{images_path}/{hh}.png')
-            hh += 1
+        # hh = 0
+        # for h in images.images:
+        #     h.save(f'{images_path}/{hh}.png')
+        #     hh += 1
         # print the iamges into a folder
         
         
-        prob_dist = classify(images_path) # should take path of images as input
+        prob_dist = classify(images.images) # should take path of images as input
+        # uniform_dist = torch.ones(1, 2).to("cuda") * 0.5
+        # torch.sum(prob_dist).backward()
+        # optimizer.step()
         '''
         {'Man': 0.8387096774193549, 'Woman': 0.16129032258064516} {'white': 0.7419354838709677, 'latino hispanic': 0.0967741935483871, 'asian': 0.06451612903225806, 'middle eastern': 0.0967741935483871}
         '''
-        print(prob_dist)
+        # print(prob_dist)
+        # prob_dist = torch.tensor(prob_dist).to("cuda")
+        # uniform_dist = torch.ones(1, 2).to("cuda") * 0.5
+        loss = prob_dist[0] * torch.log(2 * prob_dist[0]) + prob_dist[1] * torch.log(2* prob_dist[1])
+        # kl_divergence = compute_kl_divergence(prob_dist, uniform_dist)
         
-        uniform_dist = torch.ones(images.images[0].shape[0], 2) * 0.5
-        kl_divergence = compute_kl_divergence(prob_dist, uniform_dist)
-        kl_divergence.backward()
+        loss.backward()
         optimizer.step()
-        print("KL divergence: ", kl_divergence)
+        for param,name in zip(hspace_pipe.unet.deltablock.parameters(), hspace_pipe.unet.deltablock.named_parameters()):
+            print(name, param)
+        print(loss)
